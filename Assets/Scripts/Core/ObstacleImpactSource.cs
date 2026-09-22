2c8c1e3de4be868043631b752ccad45b97eddbb5
wanderwerkhoven-afk <wanderwerkhoven@gmail.com>
2026-09-22 18:10:04 +0200

    feat: add obstacle impact source

---
+++ Assets/Scripts/Core/ObstacleImpactSource.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace DontFallGranny.Core
+{
+    [RequireComponent(typeof(Collider))]
+    public sealed class ObstacleImpactSource : MonoBehaviour
+    {
+        [Header("Impact")]
+        [SerializeField, Range(0.01f, 1f)] private float balanceDamage = 0.28f;
+        [SerializeField] private bool heavyImpact;
+        [SerializeField] private float cooldownSeconds = 0.35f;
+
+        private float lastImpactTime = float.NegativeInfinity;
+
+        public float BalanceDamage => balanceDamage;
+        public bool HeavyImpact => heavyImpact;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TryHit(other);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            TryHit(collision.collider);
+        }
+
+        private void TryHit(Collider other)
+        {
+            if (Time.time - lastImpactTime < cooldownSeconds)
+                return;
+
+            if (!other.TryGetComponent<GrannyImpactReceiver>(out var receiver))
+                return;
+
+            lastImpactTime = Time.time;
+            receiver.ReceiveImpact(balanceDamage, heavyImpact, transform.position);
+        }
+    }
+}
